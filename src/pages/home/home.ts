abdebe46e28c2caf3f236d24752703ce652a899f
strim4 <moritz_strickler@msn.com>
2018-10-25 12:09:41 +0200

    Homepage Buttons working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 
+import { BeactivePage } from '../beactive/beactive';
+import { TagebuchPage } from '../tagebuch/tagebuch';
+
+import { SchmerzenmeldenPage } from '../schmerzenmelden/schmerzenmelden';
+import { SensorverbindenPage } from '../sensorverbinden/sensorverbinden';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +15,10 @@ import { NavController } from 'ionic-angular';
 
 
 export class HomePage {
+  beactivePage = BeactivePage;
+  tagebuchPage = TagebuchPage;
+  sensorverbindenPage = SensorverbindenPage;
+  schmerzenmeldenPage = SchmerzenmeldenPage;
 
   constructor(public navCtrl: NavController) {
 
