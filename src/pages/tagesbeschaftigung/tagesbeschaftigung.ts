62f284e7530b63562ad1258e0124c7dbba2c4a36
strim4 <moritz_strickler@msn.com>
2018-11-08 15:09:34 +0100

    tage v4

--- src/pages/tagesbeschaftigung/tagesbeschaftigung.ts
+++ src/pages/tagesbeschaftigung/tagesbeschaftigung.ts
@@ -15,11 +15,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TagesbeschaftigungPage {
 
+  public allList:any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.allList=[{
+      name:'NES',
+      values:'nes',
+      flag:0
+    },{
+      name:'Nintendo64',
+      values:'n64',
+      flag:0
+    },{
+      name:'PlayStation',
+      values:'ps',
+      flag:0
+    }];
   }
+
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TagesbeschaftigungPage');
   }
 
+  valuechange(key){
+  
+    for(let i = 0 ; i < this.allList.length ; i++){
+      if( this.allList[i].values==key){
+        this.allList[i].flag=1;
+      }else{
+        this.allList[i].flag=0;
+      }
+    }
+
+}
 }
