7f37125fb156bfac8ab0ca3929f5996f55db4a00
strim4 <moritz_strickler@msn.com>
2018-11-14 10:51:47 +0100

    Mediform

--- src/pages/medikamente/medikamente.ts
+++ src/pages/medikamente/medikamente.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the MedikamentePage page.
@@ -14,8 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'medikamente.html',
 })
 export class MedikamentePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private todo : FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.todo = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: [''],
+    });
+  }
+  logForm(){
+    console.log(this.todo.value)
+  
   }
 
   ionViewDidLoad() {
