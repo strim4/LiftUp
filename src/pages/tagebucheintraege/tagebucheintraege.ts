20f3d6d5a5bf0d581239b7fd61a5c583da431564
strim4 <moritz_strickler@msn.com>
2018-12-08 17:06:00 +0100

    Tagebucheinträge nach Datum anzeigen

--- src/pages/tagebucheintraege/tagebucheintraege.ts
+++ src/pages/tagebucheintraege/tagebucheintraege.ts
@@ -3,7 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
 import {FirebaseListObservable} from 'angularfire2/database-deprecated'
+
 
 import { Medi } from '../../model/medi/medi.model';
 import { Energy } from '../../model/energy/energy.model';
@@ -28,22 +31,90 @@ import {AngularFireObject} from 'angularfire2/database';
   providers: [FirebaseProvider]
 })
 export class TagebucheintraegePage {
+tdate: any;
+title: any;
+
 
   mediList: Observable<Medi[]>;
   energyList: Observable<Energy[]>;
   actlevList: Observable<Actlev[]>;
- 
+  allmediList: Observable<any>;
+  allenergyList: Observable<any>;
+  allactlevList: Observable<any>;
+
+  public filteredMediList: Observable<any> ;
+  public filteredTextMediList: Observable<any> ;
+  public filteredEnergyList: Observable<any> ;
+  filteredActlevelList: Observable<any> ;
+
+  public aMediList: Observable<any> ;
+  
+  
 
   
   constructor(public navCtrl: NavController, public afd: AngularFireDatabase,private firebaseProvider: FirebaseProvider, public navParams: NavParams) {
-
+   // this.tdate = tdate;
 this.mediList = afd.list<Medi>('medi-list').valueChanges();
+
 this.energyList = afd.list<Energy>('energy-list').valueChanges();
 this.actlevList = afd.list<Actlev>('actlev-list').valueChanges();
+/*this.filteredMediList = this.mediList
+      .map(receita => {        
+        return receita.filter(r => {          
+          return (r.date) == this.tdate;
+        })
+      }, error => console.log(error)); */
+
+
+      
 
 
   }
 
+ 
+
+//working with title
+ /*showFilter(title: string){
+  this.filteredTextMediList = this.mediList
+  .map(receita => {        
+    return receita.filter(r => {          
+      return (r.title) == this.title;
+    })
+  }, error => console.log(error))
+ }*/
+//Filter für das Datum der Tagebucheinträge
+ showFilterDate(date: Date){
+  this.filteredTextMediList = this.mediList
+  .map(receita => {        
+    return receita.filter(r => {          
+      return (r.date) == this.tdate;
+    })
+  }, error => console.log(error));
+  this.filteredEnergyList = this.energyList
+  .map(receita => {        
+    return receita.filter(r => {          
+      return (r.date) == this.tdate;
+    })
+  }, error => console.log(error));
+  this.filteredActlevelList = this.actlevList
+  .map(receita => {        
+    return receita.filter(r => {          
+      return (r.date) == this.tdate;
+    })
+  }, error => console.log(error));
+
+
+ }
+
+ showAll(){
+  this.allmediList = this.mediList;
+  this.allenergyList = this.energyList;
+  this.allactlevList = this.actlevList;
+
+
+ }
+
+ 
   ionViewDidLoad() {
     
     console.log('ionViewDidLoad TagebucheintraegePage');
