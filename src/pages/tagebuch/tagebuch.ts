653ba5fa1ccc711b3cd239bc361c1289fe5c3780
zbinc2 <32926166+zbinc2@users.noreply.github.com>
2019-01-09 14:28:00 +0100

    InfoButton Tagebuch

--- src/pages/tagebuch/tagebuch.ts
+++ src/pages/tagebuch/tagebuch.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 import { TagesbeschaftigungPage } from '../tagesbeschaftigung/tagesbeschaftigung';
 import { EnergiePage } from '../energie/energie';
 import { AktivitaetslevelPage } from '../aktivitaetslevel/aktivitaetslevel';
@@ -29,18 +29,20 @@ export class TagebuchPage {
   medikamentePage = MedikamentePage;
   tagebucheintraegePage = TagebucheintraegePage;
 
-
- 
- 
- 
-
- 
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TagebuchPage');
+  }
+
+  btnClickedInfo(){
+    let alert = this.alertCtrl.create({
+      title: 'Information',
+      message: 'Erstellen Sie hier Ihre Tagebucheinträge. Was haben Sie vorgängig am Tag gemacht, wie aktiv waren Sie? Haben Sie Medikamente genommen? Die Angaben werden für die statistische Auswertung benutzt. Diese finden Sie unter Analyse.',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
